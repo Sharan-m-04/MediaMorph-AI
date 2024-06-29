f2523878ae44876ed90d1ec4d49398f4318cd72c
Sharan M <101189789+Sharan-m-04@users.noreply.github.com>
2024-06-29 22:45:24 +0530

    Update views.py

--- MediaMorphAI/Landing/views.py
+++ MediaMorphAI/Landing/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 def landing(request):
     # TODO: HANDLE CONTACT FORM TO SUBMIT THE DATA TO THE DATABASE
     return render(request, 'index.html')
