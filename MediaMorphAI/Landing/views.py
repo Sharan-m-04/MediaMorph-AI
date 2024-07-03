06afa425b396b5859d333c5de49ed95fdda20aa9
Sharan M <msharan.hnp@gmail.com>
2024-07-03 22:28:10 +0530

    added logo, fixed feedbacks, db registration to admin page, code improvements

--- MediaMorphAI/Landing/views.py
+++ MediaMorphAI/Landing/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import redirect, render
 from Feedbacks.models import Feedback
 
 def landing(request):
-    # TODO: HANDLE CONTACT FORM TO SUBMIT THE DATA TO THE DATABASE
     if request.method == 'POST':
+        isSaved = False
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
         new_feedback = Feedback(name=name, email=email, message=message)
         new_feedback.save()
-        return redirect('/')
+        isSaved = True
+        return redirect('/', {'isSaved':isSaved})
     else:
         return render(request, 'index.html')
