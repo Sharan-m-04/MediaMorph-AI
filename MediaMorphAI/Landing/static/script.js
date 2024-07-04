115ad99d5baecbf8d21fda0a3408a883cfd6cbbd
Sharan.m.04 <101189789+Sharan-m-04@users.noreply.github.com>
2024-07-04 16:14:08 +0530

    responsive in progress

--- MediaMorphAI/Landing/static/script.js
+++ MediaMorphAI/Landing/static/script.js
@@ -1,28 +1,35 @@
-function toggleMenu() {
-        document.querySelector('.nav-links').classList.toggle('active');
-        document.querySelector('.hamburger').classList.toggle('active');
+// const header = document.querySelector(".header");
+
+// navLinks.addEventListener("click", () => {
+//   header.classList.toggle("header-expanded");
+// });
+
+// function toggleMenu() {
+//   const navLinks = document.querySelectorAll(".nav-links");
+//   navLinks.forEach((nav) => nav.classList.toggle("active"));
+//   document.querySelector(".hamburger").classList.toggle("active");
+// }
+
+function loginBtn() {
+  window.open("/accounts/login", (target = "_self"));
 }
 
-function loginBtn(){
-    window.open("/accounts/login", target='_self');
-}
-
-function getstarted(){
-    window.open("/accounts/signup", target='_self');
+function getstarted() {
+  window.open("/accounts/signup", (target = "_self"));
 }
 
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
 
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
 
         if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
+            reveals[i].classList.add("acti");
         } else {
-            reveals[i].classList.remove("active");
+            reveals[i].classList.remove("acti");
         }
     }
 }
