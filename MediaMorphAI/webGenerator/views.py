bc25720e404f1adc798afd06fe5e3f0e9892d8ee
Sharan M <msharan.hnp@gmail.com>
2024-09-19 19:59:49 +0530

    tried some new logic for data transfer, some bug fixes

--- MediaMorphAI/webGenerator/views.py
+++ MediaMorphAI/webGenerator/views.py
@@ -1,3 +1,4 @@
+import os
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.conf import settings
@@ -16,26 +17,33 @@ def generateweb(request):
         responses_data = json.loads(responses)
         prompt = f'''create a responsive website using html, css and js, put it in a single html file, Use fontawsome for icons, make the best design and animations possible. Just give the code, no explanations or summary please. where ever image is required use the alt as "Image Here". Don't use markdown for the code, just give the raw text. The exact requirements are given in the following json: {responses_data}'''
 
+        print(responses_data)
+
         response = openai.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": prompt}]
         )
+
+        print(source_code)
+
         source_code = response.choices[0].message.content.strip()
-        request.session['source_code'] = source_code
-        # return JsonResponse({'source_code': source_code})
-        # print(f"Generated Source Code: {source_code}", flush=True)
-        return redirect('preview.html')
+        with open(os.path.join(settings.BASE_DIR, 'webGenerator', 'generated_source_code.txt'), 'w') as file:
+            file.write(source_code)
+
+        return JsonResponse({'success': True})
     else:
         return render(request, 'webGenerator.html')
 
 @csrf_exempt
 @login_required
 def preview (request):
-    source_code = request.session.get('source_code', '')
-    if not source_code:
-        source_code = '<h1>Something went wrong :( <br> please try again later!</h1>'
+    try:
+        with open(os.path.join(settings.BASE_DIR, 'webGenerator', 'generated_source_code.txt'), 'r') as file:
+            source_code = file.read()
+    except FileNotFoundError:
+        source_code = '<h1>Something went wrong :(</h1>'
+    
     return render(request, 'preview.html', {'source_code': source_code})
-    # return render(request, 'preview.html')
 
 
 
