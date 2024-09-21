05b910b70b16a85a09945f35a87d797d61ba4ebd
Sharan M <msharan.hnp@gmail.com>
2024-09-21 12:43:32 +0530

    bugs fixed, web generating properly now

--- MediaMorphAI/webGenerator/views.py
+++ MediaMorphAI/webGenerator/views.py
@@ -37,13 +37,33 @@ def generateweb(request):
 @csrf_exempt
 @login_required
 def preview (request):
-    try:
-        with open(os.path.join(settings.BASE_DIR, 'webGenerator', 'generated_source_code.txt'), 'r') as file:
-            source_code = file.read()
-    except FileNotFoundError:
-        source_code = '<h1>Something went wrong :(</h1>'
+    if request.method == 'POST':
+        openai.api_key = settings.SECRET_KEY
+        responses = request.POST.get('responses')
+        responses_data = json.loads(responses)
+        prompt = f'''create a responsive website using html, css and js, put it in a single html file, Use fontawsome for icons, make the best design and animations possible. Just give the code, no explanations or summary please. where ever image is required use the alt as "Image Here". Don't use markdown for the code, just give the raw text. The exact requirements are given in the following json: {responses_data}'''
+
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}]
+        )
+
+        source_code = response.choices[0].message.content.strip()
+        # with open(os.path.join(settings.BASE_DIR, 'webGenerator', 'generated_source_code.txt'), 'w') as file:
+        #     file.write(source_code)
+
+        # return JsonResponse({'success': True})
+        # try:
+        #     with open(os.path.join(settings.BASE_DIR, 'webGenerator', 'generated_source_code.txt'), 'r') as file:
+        #         source_code = file.read()
+        # except FileNotFoundError:
+        #     source_code = '<h1>Something went wrong :(</h1>'
     
-    return render(request, 'preview.html', {'source_code': source_code})
+        return render(request, 'preview.html', {'source_code': source_code})
+    else:
+        source_code = '<h1>Something went wrong :(</h1>'
+        return render(request, 'preview.html', {'source_code': source_code})
+
 
 
 
